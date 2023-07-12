382e7f6af20211915a52aa436b6d4848c10c8558
Aysha  P A <aysha.p-a@tnpconsultants.com>
2023-07-12 09:57:51 +0530

    changesincommunity

--- src/app/modules/profile/community/community.component.ts
+++ src/app/modules/profile/community/community.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./community.component.css']
 })
 export class CommunityComponent {
+ community:any=[{
+  "name":"SoulDEVS",
+  "description":"The lost souls of developers 👻"
+ },
+ {
+  "name":"🎨 Designers",
+  "description":"Magicians of all things visual"
+ },
+ {
+  "name":"TNP Alerts",
+  "description":"Official announcements, info or news related to organization"
+ },
+ {
+  "name":"Digital Team",
+  "description":"Byte-sized Brilliance"
+ },
+ {
+  "name":"Flavor Fiesta",
+  "description":"Nothing but food🍔🍕🍟🧆"
+ }];
 
+
+ 
 }
